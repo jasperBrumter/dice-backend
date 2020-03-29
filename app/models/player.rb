82ae865605695eebd73243647dacf92adfe803d6
Jasper Brumter <jasper.brumter@gmail.com>
2020-03-29 17:23:27 -0400

    Fix Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,8 @@
 class Player < ApplicationRecord
   belongs_to :game
 
-  validates :name, presence: true,
-  uniqueness: { scope: :game,
-    message: "A player has already picked that name" }
+  validates :name, presence: true, uniqueness: { scope: :game, message: "A player has already picked that name" }
   validates :order, uniqueness: true
-  validates :current_hand, presence: true,
+  validates :current_hand, presence: true
   validates :number_dice, presence: true
 end
